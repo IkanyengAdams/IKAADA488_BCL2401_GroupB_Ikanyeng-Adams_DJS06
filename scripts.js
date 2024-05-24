91e8112031f928981368f9994daeb6ad93d801bb
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-05-24 09:58:55 +0200

    created a html file to access the console on the browser and used the forEach method to log the name and province on the console

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 // A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
 
 // A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
 
 // A list of products with prices:
 const products = [
@@ -12,4 +12,12 @@ const products = [
   { product: 'avocado', price: "8" },
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
-]
+];
+
+//Using the forEach method to match a name and a specific province
+
+names.forEach((name, index) => {
+  console.log(name);
+  console.log(provinces[index]);
+  console.log(`${name} (${provinces[index]})`);
+});
